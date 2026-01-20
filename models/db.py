1e69edd8fd42ca7a7e6d95212ceff01bd614f9e7
ravitejagandhamalla <ravitejagandhamalla@gmail.com>
2026-01-20 23:29:03 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -2,7 +2,10 @@ import os
 import psycopg2
 import psycopg2.extras
 
-DATABASE_URL = os.environ.get("postgresql://postgres.vjmksejmnxgowpnwgaxv:Ravi141645%24@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
+DATABASE_URL = os.getenv("postgresql://postgres.vjmksejmnxgowpnwgaxv:Ravi141645DB@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in environment variables")
 
 def get_db_connection():
     return psycopg2.connect(

1e3df1ff7ca56bd0be4c22c293201fcaef14b6b0
ravitejagandhamalla <ravitejagandhamalla@gmail.com>
2026-01-20 23:20:49 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 import psycopg2.extras
 
-DATABASE_URL = os.environ.get("postgresql://postgres.vjmksejmnxgowpnwgaxv:Ravi141645$@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
+DATABASE_URL = os.environ.get("postgresql://postgres.vjmksejmnxgowpnwgaxv:Ravi141645%24@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
 
 def get_db_connection():
     return psycopg2.connect(

8cf4e4cba88243766947a0fcf0492cf8f334961c
ravitejagandhamalla <ravitejagandhamalla@gmail.com>
2026-01-21 14:31:43 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,17 +1,24 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg2.extras
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from dotenv import load_dotenv
+from werkzeug.security import check_password_hash
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "super-secret-key-123")
+
+DATABASE_URL = os.getenv("postgresql://postgres.vjmksejmnxgowpnwgaxv:Rohiit141645@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
 
 def get_db_connection():
-    database_url = os.getenv("postgresql://postgres.vjmksejmnxgowpnwgaxv:dlmHeBHcR0IWx8Jm@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
-
-    if not database_url:
-        # Do NOT crash the app on import
-        raise RuntimeError("DATABASE_URL environment variable is missing")
+    if not DATABASE_URL:
+        print("⚠️ DATABASE_URL not found in environment variables")
+        return None
 
     return psycopg2.connect(
-        database_url,
-        sslmode="require",
-        cursor_factory=RealDictCursor
+        DATABASE_URL,
+        cursor_factory=psycopg2.extras.RealDictCursor,
+        sslmode="require"
     )
-

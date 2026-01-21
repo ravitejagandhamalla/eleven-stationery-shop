5ef94f61f8db722b7793755cc4d407b1058687fd
ravitejagandhamalla <ravitejagandhamalla@gmail.com>
2026-01-21 12:33:54 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,15 +1,17 @@
 import os
 import psycopg2
-import psycopg2.extras
-
-DATABASE_URL = os.getenv("postgresql://postgres.vjmksejmnxgowpnwgaxv:Rohiit141645@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set")
+from psycopg2.extras import RealDictCursor
 
 def get_db_connection():
+    database_url = os.getenv("postgresql://postgres.vjmksejmnxgowpnwgaxv:dlmHeBHcR0IWx8Jm@aws-1-ap-south-1.pooler.supabase.com:5432/postgres")
+
+    if not database_url:
+        # Do NOT crash the app on import
+        raise RuntimeError("DATABASE_URL environment variable is missing")
+
     return psycopg2.connect(
-        DATABASE_URL,
-        cursor_factory=psycopg2.extras.RealDictCursor,
-        sslmode="require"
+        database_url,
+        sslmode="require",
+        cursor_factory=RealDictCursor
     )
+
